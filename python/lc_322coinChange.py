78edbdc8b3bc2226e25c149bc874045f960bcdfc
npicciano79 <npicciano79@gmail.com>
2023-02-25 17:07:49 -0500

    lc 322 coin change solution

---
+++ python/lc_322coinChange.py
@@ -0,0 +1,24 @@
+#322 coin change
+"""
+---given an array of integers, coins, of different denominations and integer amount
+---return the fewest number of coins needed to makeup that amount
+"""
+
+class Solution(object):
+    def coinChange(self,coins, amount):
+        dp=[amount+1]*(amount+1)
+        dp[0]=0
+
+        for a in range(1,amount+1):
+            for c in coins:
+                #amount of money is larger than the coin 
+                #coin can be subtracted from amount 
+                if a-c>=0:
+                    dp[a]=min(dp[a],1+dp[a-c])
+        return dp[amount] if dp[amount]!=amount+1 else -1
+
+
+coins=[1,2,5]
+amount=11
+solution=Solution()
+print(solution.coinChange(coins, amount))
