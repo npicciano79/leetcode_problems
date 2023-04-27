f56b1d6e5a5b84ab7ab7caf8d574ee4e986dd572
npicciano79 <npicciano79@gmail.com>
2023-04-27 07:08:01 -0400

    lc 1929 concatenation of arrays solution

---
+++ python/lc_1929concatArray.py
@@ -0,0 +1,22 @@
+#1929 concatenation of arrays
+"""
+---given an array of ints nums, of length n, create an array ans
+---of length 2n, where ans[i]=nums[i] and ans[i+n]=nums[i]
+---ans is the concatenation of 2 nums arrays
+"""
+
+class Solution(object):
+    def getConcatenation(self,nums):
+        ans=[]
+        for i in range(2):
+            for num in nums:
+                ans.append(num)
+
+        return ans
+
+
+
+
+nums=[1,2,1]
+solution=Solution()
+print(solution.getConcatenation(nums))
