5985c6b92d7c5d2b1ea19a793e5848972f471b2a
npicciano79 <npicciano79@gmail.com>
2023-05-06 06:42:18 -0400

    lc 2418 updated solution to sort people problem

--- python/lc_2418sortPeople.py
+++ python/lc_2418sortPeople.py
@@ -7,8 +7,25 @@
 ---return names sorted in decending order by the peoples heights
 """
 
+"""
+---original solution: runtime: 607ms beats 14% memory 13.9 beats 95%
+---updated solution : runtine 206ms beates 20% memory 13.9 beats 95%
+"""
+
 class Solution(object):
     def sortPeople(self,names,heights):
+        while heights:
+            heightMaxIndex=heights.index(max(heights))
+            #pop previous max from heights
+            heights.pop(heightMaxIndex)
+            #heights[heightMaxIndex]=float('-inf')
+            #print(heightMaxIndex)
+            names.append(names.pop(heightMaxIndex))
+            #print(names)
+            
+        return names
+
+        """
         res=[]
         while len(res)!=len(names):
             tempMaxHeight=max(heights)
@@ -16,6 +33,7 @@ class Solution(object):
             heights[heights.index(tempMaxHeight)]=float('-inf')
         
         return res
+        """
     
 names = ["Mary","John","Emma"]
 heights = [180,165,170]
