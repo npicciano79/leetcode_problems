5310ac1e06cd447f1bbe69afc181ec573bcc0918
npicciano79 <npicciano79@gmail.com>
2023-06-05 09:39:02 -0400

    solution lc 1141 user activity past 30 days

---
+++ sql/lc_1141userActivityPast30Days.sql
@@ -0,0 +1,38 @@
+---1141 user activity for hte past 30 days 1
+/*
+Activity table:
++---------+------------+---------------+---------------+
+| user_id | session_id | activity_date | activity_type |
++---------+------------+---------------+---------------+
+| 1       | 1          | 2019-07-20    | open_session  |
+| 1       | 1          | 2019-07-20    | scroll_down   |
+| 1       | 1          | 2019-07-20    | end_session   |
+| 2       | 4          | 2019-07-20    | open_session  |
+| 2       | 4          | 2019-07-21    | send_message  |
+| 2       | 4          | 2019-07-21    | end_session   |
+| 3       | 2          | 2019-07-21    | open_session  |
+| 3       | 2          | 2019-07-21    | send_message  |
+| 3       | 2          | 2019-07-21    | end_session   |
+| 4       | 3          | 2019-06-25    | open_session  |
+| 4       | 3          | 2019-06-25    | end_session   |
++---------+------------+---------------+---------------+
+Output: 
++------------+--------------+ 
+| day        | active_users |
++------------+--------------+ 
+| 2019-07-20 | 2            |
+| 2019-07-21 | 2            |
+
+
+Write an SQL query to find the daily active user count for a 
+period of 30 days ending 2019-07-27 inclusively. A user was 
+active on someday if they made at least one activity on that day.
+Return the result table in any order.
+
+*/
+
+
+SELECT activity_date AS day,COUNT(DISTINCT user_id) AS active_users
+FROM activity
+WHERE activity_date>'2019-06-27' AND activity_date<='2019-07-27'
+GROUP BY activity_date;
