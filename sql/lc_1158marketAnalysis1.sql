dd6c51623249dbb0bb76172d8981abc7fd34dd52
npicciano79 <npicciano79@gmail.com>
2023-06-08 10:21:42 -0400

    lc 1158 market analysis 1 solution

---
+++ sql/lc_1158marketAnalysis1.sql
@@ -0,0 +1,52 @@
+---1158 market analysis1
+/*
+Input: 
+Users table:
++---------+------------+----------------+
+| user_id | join_date  | favorite_brand |
++---------+------------+----------------+
+| 1       | 2018-01-01 | Lenovo         |
+| 2       | 2018-02-09 | Samsung        |
+| 3       | 2018-01-19 | LG             |
+| 4       | 2018-05-21 | HP             |
++---------+------------+----------------+
+Orders table:
++----------+------------+---------+----------+-----------+
+| order_id | order_date | item_id | buyer_id | seller_id |
++----------+------------+---------+----------+-----------+
+| 1        | 2019-08-01 | 4       | 1        | 2         |
+| 2        | 2018-08-02 | 2       | 1        | 3         |
+| 3        | 2019-08-03 | 3       | 2        | 3         |
+| 4        | 2018-08-04 | 1       | 4        | 2         |
+| 5        | 2018-08-04 | 1       | 3        | 4         |
+| 6        | 2019-08-05 | 2       | 2        | 4         |
++----------+------------+---------+----------+-----------+
+Items table:
++---------+------------+
+| item_id | item_brand |
++---------+------------+
+| 1       | Samsung    |
+| 2       | Lenovo     |
+| 3       | LG         |
+| 4       | HP         |
++---------+------------+
+Output: 
++-----------+------------+----------------+
+| buyer_id  | join_date  | orders_in_2019 |
++-----------+------------+----------------+
+| 1         | 2018-01-01 | 1              |
+| 2         | 2018-02-09 | 2              |
+| 3         | 2018-01-19 | 0              |
+| 4         | 2018-05-21 | 0              |
++-----------+------------+----------------+
+
+
+Write an SQL query to find for each user, the join date 
+and the number of orders they made as a buyer in 2019.
+*/
+
+SELECT u.user_id AS buyer_id,u.join_date,COUNT(o.order_id)AS orders_in_2019
+FROM users AS u LEFT JOIN orders AS o
+ON o.buyer_id=u.user_id
+AND o.order_date LIKE '2019%'
+GROUP BY u.user_id;
