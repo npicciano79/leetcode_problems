df6cd814de8bfb5f650138374aca2e42a293f178
npicciano79 <npicciano79@gmail.com>
2023-06-09 11:21:31 -0400

    lc 1532 most recent 3 orders solution

---
+++ sql/lc_1532mostRecent3Orders.sql
@@ -0,0 +1,60 @@
+---1532 most recent 3 orders 
+/*
+Customers table:
++-------------+-----------+
+| customer_id | name      |
++-------------+-----------+
+| 1           | Winston   |
+| 2           | Jonathan  |
+| 3           | Annabelle |
+| 4           | Marwan    |
+| 5           | Khaled    |
++-------------+-----------+
+Orders table:
++----------+------------+-------------+------+
+| order_id | order_date | customer_id | cost |
++----------+------------+-------------+------+
+| 1        | 2020-07-31 | 1           | 30   |
+| 2        | 2020-07-30 | 2           | 40   |
+| 3        | 2020-07-31 | 3           | 70   |
+| 4        | 2020-07-29 | 4           | 100  |
+| 5        | 2020-06-10 | 1           | 1010 |
+| 6        | 2020-08-01 | 2           | 102  |
+| 7        | 2020-08-01 | 3           | 111  |
+| 8        | 2020-08-03 | 1           | 99   |
+| 9        | 2020-08-07 | 2           | 32   |
+| 10       | 2020-07-15 | 1           | 2    |
++----------+------------+-------------+------+
+Output: 
++---------------+-------------+----------+------------+
+| customer_name | customer_id | order_id | order_date |
++---------------+-------------+----------+------------+
+| Annabelle     | 3           | 7        | 2020-08-01 |
+| Annabelle     | 3           | 3        | 2020-07-31 |
+| Jonathan      | 2           | 9        | 2020-08-07 |
+| Jonathan      | 2           | 6        | 2020-08-01 |
+| Jonathan      | 2           | 2        | 2020-07-30 |
+| Marwan        | 4           | 4        | 2020-07-29 |
+| Winston       | 1           | 8        | 2020-08-03 |
+| Winston       | 1           | 1        | 2020-07-31 |
+| Winston       | 1           | 10       | 2020-07-15 |
++---------------+-------------+----------+------------+
+Write an SQL query to find the most recent three orders of each user. 
+If a user ordered less than three orders, return all of their orders.
+
+Return the result table ordered by customer_name in ascending order and 
+in case of a tie by the customer_id in ascending order. If there is still a 
+tie, order them by order_date in descending order.
+
+
+*/
+
+with cte AS(
+  SELECT * ,RANK () OVER(PARTITION BY customer_id ORDER BY order_date DESC) AS t
+  FROM ORDERS  
+)
+SELECT c.name AS customer_name,t.customer_id,t.order_id,t.order_date
+FROM cte AS t LEFT JOIN customers AS c
+ON c.customer_id=t.customer_id
+WHERE t.t<=3
+ORDER BY c.name ASC,t.customer_id ASC, t.order_date DESC;
